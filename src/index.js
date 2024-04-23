679e6e146fef334c4218688ed0c192dfa40cad47
Allen Lee <6207ALS@gmail.com>
2024-04-23 14:47:34 -0400

    Stop rrweb when new recording
    Co-authored-by: Weston Ludeke <westonludeke@gmail.com>
    Co-authored-by: Rachele Lang <rachelelang7672@gmail.com>
    Co-authored-by: Tess Lockey <84677774+treskey@users.noreply.github.com>

---
+++ src/index.js
@@ -0,0 +1,47 @@
+import * as rrweb from "rrweb";
+
+class CampfireSDK {
+	constructor() {
+		this.init();
+	}
+
+	initRrweb() {
+		if (window) {
+			window.addEventListener("message", (e) => {
+				const validOriginRegExp = /https:\/\/campfire\.(.*)/
+				if (validOriginRegExp.test(e.origin)) {
+					if (this.stopFn) this.stopFn()
+					this.stopFn = new rrweb.record({
+						emit() {},
+						recordCrossOriginIframes: true,
+					})
+				}
+			})
+		}
+	}
+
+	initCSSEditor() {
+		document.addEventListener("mouseover", (e) => {
+			e.target.style.border = "#9ecaed";
+			e.target.style.boxShadow = "0 0 10px #9ecaed";
+		})
+
+		document.addEventListener("mouseout", (e) => {
+			if (e.target !== document.body && e.target instanceof HTMLElement) {
+				e.target.style.border = "";
+				e.target.style.boxShadow =  "";
+			}
+		})
+
+		document.addEventListener("click", (e) => {
+			
+		});
+	}
+
+	init() {
+		this.initRrweb();
+		// this.initCSSEditor();
+	}
+}
+
+export default CampfireSDK;
---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export default {
+  mode: 'production',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+		library: {
+			type: "module"
+		}
+  },
+	experiments: {
+		outputModule: true,
+	},
+};
